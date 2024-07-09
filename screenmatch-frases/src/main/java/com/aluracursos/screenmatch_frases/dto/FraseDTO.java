30391f0bb1fbd7444a99a5842bafba190d6f9c1c
Jhonatan <jhonatanhuaman76@gmail.com>
2024-07-09 08:37:57 -0500

    Preparando objeto DTO

---
+++ screenmatch-frases/src/main/java/com/aluracursos/screenmatch_frases/dto/FraseDTO.java
@@ -0,0 +1,10 @@
+package com.aluracursos.screenmatch_frases.dto;
+
+public record FraseDTO(
+        Long id,
+        String titulo,
+        String frase,
+        String personaje,
+        String poster
+) {
+}
